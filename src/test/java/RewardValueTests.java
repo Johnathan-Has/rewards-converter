7e26de2d1dca35974434820299c79fe9ce2f7db3
Johnathan <jonathanhas9@gmail.com>
2023-07-04 20:39:44 -0400

    Added Unit tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 10;
+        RewardValue reward = new RewardValue(cashValue);
+        double expectedMilesValue = cashValue / .0035;
+        assertEquals(expectedMilesValue, reward.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double milesValue = 10;
+        RewardValue reward = new RewardValue((int) milesValue);
+        double expectedCashValue = milesValue * 0.0035;
+        assertEquals(expectedCashValue, reward.getCashValue());
     }
 }
